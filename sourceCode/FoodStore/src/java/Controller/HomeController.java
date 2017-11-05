7112e204638e15a7d00cf6993df999ea1ba78116
ducanh <rogerducanh@gmail.com>
2017-11-06 00:37:19 +0700

    add source code

---
+++ sourceCode/FoodStore/src/java/Controller/HomeController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Duc Anh
+ */
+@Controller
+@RequestMapping(value = "/home")
+public class HomeController {
+    @RequestMapping(value = "/index")
+    public String Index(ModelMap mm){
+        mm.put("aa", "xin chao cac ban");
+        return "pages/index";
+    }
+}
